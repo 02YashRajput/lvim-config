08dce9986f251d92a95fd885a1ef0a420f76ad3e
yash.rajput.001 <yash.rajput.001@juspay.in>
2025-11-21 16:17:01 +0530

    initial commit

---
+++ lua/user/settings.lua
@@ -0,0 +1,15 @@
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    -- Only open if no file is passed
+    if vim.fn.argc() == 0 then
+      require("nvim-tree.api").tree.open()
+    end
+  end
+})
+
+-- Optional: NvimTree settings
+lvim.builtin.nvimtree.setup.view.side = "left"
+lvim.builtin.nvimtree.setup.view.width = 30
+lvim.builtin.nvimtree.setup.renderer.icons.show.git = true
+
